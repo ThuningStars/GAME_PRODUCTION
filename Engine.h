975e8c6ff5338a125e3658cfd2fd5f6bf30d6ec4
Snomra <59655904+Snomra@users.noreply.github.com>
2021-02-02 09:53:23 -0500

    Add files via upload
    Added additional platforms to level

---
+++ Engine.h
@@ -0,0 +1,70 @@
+#pragma once
+#ifndef _ENGINE_H_
+#define _ENGINE_H_
+
+#include "SDL.h"
+#include <iostream>
+#include <vector>
+
+#include "PlatformPlayer.h"
+#include "PlayerBullet.h"
+#include "Enemy.h"
+#define FPS 60
+#define WIDTH 1024
+#define HEIGHT 768
+#define LEVEL_WIDTH 1500
+#define LEVEL_HEIGHT 768
+
+
+using namespace std;
+
+class Engine
+{
+private: // private properties.
+	bool m_running = false;
+	Uint32 m_start, m_end, m_delta, m_fps;
+	const Uint8* m_keystates;
+	SDL_Window* m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	SDL_Rect m_Platforms[8] = {	{100,550,100,20},//0					
+									{200,500,100,20},//1
+									{300,400,100,20},//2
+									{400,700,100,20},//3
+									{0,700,10240,100},//4. Ground
+									{500,600,75,210},//5
+									{700,500,100,20},//6
+									{900,400,200,40},//7
+	}; //Position X and y, width and height
+
+	SDL_Rect m_Camera = { 100, 0, WIDTH, HEIGHT };
+	PlatformPlayer m_player;
+	vector<Bullet*> m_playerbullet;
+	vector<LeftBullet*> m_playerleftbullet;
+	SDL_Texture* m_playerIdleTexture, *m_playerRunTexture;
+	SDL_RendererFlip flip = SDL_FLIP_NONE;
+	Sprite m_enemy;
+	vector<Enemy*> m_enemyCreation;
+	int m_EnemyTimer;
+
+	// Textures
+	SDL_Texture* m_pTexture;
+
+
+private: // private method prototypes.
+	int Init(const char* title, int xPos, int yPos, int width, int height, int flags);
+	void Clean();
+	void Wake();
+	void HandleEvents();
+	bool KeyDown(const SDL_Scancode c);
+	void CheckCollision();
+	void Update();
+	void Render();
+	void Sleep();
+
+public: // public method prototypes.
+	int Run();
+};
+
+#endif
+
+// Reminder: you can ONLY have declarations in headers, not logical code
